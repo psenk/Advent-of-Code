fe39cbe06b2947fbb75fef41b874850431b0671b
Paul Senkbeil <psenk168@gmail.com>
2023-12-06 00:40:42 -0600

    Added Day Three file.  Adjusted main for day three challenge.

--- Code/Main.java
+++ Code/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         //Day day = new DayOne();
-        Day day = new DayTwo();
+        //Day day = new DayTwo();
+        Day day = new DayThree();
         day.compute();
     }
 }
