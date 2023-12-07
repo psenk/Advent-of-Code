e35922cd798476d5006ea644b45682de75d53392
Paul Senkbeil <psenk168@gmail.com>
2023-12-06 18:39:26 -0600

    Created project, moved all files into appropriate folders.

--- src/DayFour.java
+++ src/DayFour.java
@@ -1,19 +1,38 @@
+package AdventOfCode;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
+import Util.Tuple;
+
 public class DayFour implements Day {
     HashSet<Integer> winningNumSet = new HashSet<>();
-    int totalPoints = 0;
+    HashMap<String, Integer> numCards = new HashMap<>();
+    //int totalPoints = 0; // 1st star
+    int gameNumber = 0;
 
     public void compute() {
         try (Scanner sc = new Scanner(new File(".\\Day Four.txt"))) {
             while (sc.hasNextLine()) {
-                int cardPoints = 0;
+                // scanning the ticket
+                //int cardPoints = 0; // 1st star
+                gameNumber++;
+                int numMatches = 0;
                 String lottoTicket = sc.nextLine();
+                // making dictionary for amt of times card is drawn
+                if (!numCards.containsKey(lottoTicket)) {
+                    numCards.put(lottoTicket, numMatches);
+                }
+                else {
+                    numCards.put(lottoTicket, numMatches + 1);
+                }
                 int winningNumbersIndex = lottoTicket.indexOf(":") + 1;
                 int ticketNumbersIndex = lottoTicket.indexOf("|") + 1;
+                
+                // winning numbers
                 String[] winningNums = lottoTicket.substring(winningNumbersIndex, ticketNumbersIndex - 1).strip().split(" ");
                 // TODO: get rid of the "" elements in winningNums before loop
                 for (int i = 0; i < winningNums.length; i++) {
@@ -21,17 +40,20 @@ public class DayFour implements Day {
                         winningNumSet.add(Integer.valueOf(winningNums[i]));
                     }
                 }
+
+                // card numbers
                 String[] ticketNumbers = lottoTicket.substring(ticketNumbersIndex).strip().split(" ");
                 for (int i = 0; i < ticketNumbers.length; i++) {
                     if (ticketNumbers[i] != "") {
                         if (winningNumSet.contains(Integer.valueOf(ticketNumbers[i]))) {
-                            cardPoints = (cardPoints == 0) ? 1 : cardPoints << 1;
+                            numMatches++;
+                            //cardPoints = (cardPoints == 0) ? 1 : cardPoints << 1; // 1st star
                             //System.out.println(totalPoints);
                             //System.out.print(Integer.valueOf(ticketNumbers[i]) + " ");
                         }
                     }
                 }
-                totalPoints += cardPoints;
+                //totalPoints += cardPoints; // 1st star
                 winningNumSet.clear();
                 //System.out.println();
             }
