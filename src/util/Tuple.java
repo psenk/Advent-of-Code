c14e606f8724382c2a8efb13ac1038d72c0e4505
Paul Senkbeil <psenk168@gmail.com>
2023-12-06 21:57:17 -0600

    Relocated to new inputs folder.

--- src/util/Tuple.java
+++ src/util/Tuple.java
@@ -1,5 +1,30 @@
-package Util;
+package util;
 
-public class Tuple {
-    
+public class Tuple<A, B> implements Pair<A, B> {
+
+    private A key;
+    private B value;
+
+    public Tuple(A key, B value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public A getKey() {
+        return this.key;
+    }
+
+    @Override
+    public B getValue() {
+        return this.value;
+    }
+
+    protected void setKey(A key) {
+        this.key = key;
+    }
+
+    protected void setValue(B value) {
+        this.value = value;
+    }
 }
