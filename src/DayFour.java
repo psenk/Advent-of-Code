f7299f2cab5a17c81d496fcb3bb215318e0b28e9
Paul Senkbeil <psenk168@gmail.com>
2023-12-07 14:14:24 -0600

    Day Four 2nd Star Completed

--- src/DayFour.java
+++ src/DayFour.java
@@ -1,68 +1,129 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
-import util.Tuple;
-
 public class DayFour implements Day {
-    boolean gameRunning = true;
-    HashSet<Integer> winningNumSet = new HashSet<>();
-    HashMap<String, Integer> numCards = new HashMap<>();
-    //int totalPoints = 0; // 1st star
+
+    // 9721255
+
+    HashMap<Integer, Integer> dataSet = new HashMap<>();
+    HashMap<Integer, Integer> numCards = new HashMap<>();
+    ArrayList<Integer> cardQueue = new ArrayList<>();
+    // int totalPoints = 0; // 1st star
     int gameNumber = 0;
+    int totalCards = 0;
+
+    public int scanCard(String lottoCard) {
+        int numMatches = 0;
+        int winningNumbersIndex = lottoCard.indexOf(":") + 1;
+        int cardNumbersIndex = lottoCard.indexOf("|") + 1;
+        HashSet<Integer> winningNumSet = new HashSet<>();
+        // winning numbers
+        String[] winningNums = lottoCard.substring(winningNumbersIndex, cardNumbersIndex - 1).strip()
+                .split(" ");
+        // TODO: get rid of the "" elements in winningNums before loop
+        for (int i = 0; i < winningNums.length; i++) {
+            if (winningNums[i] != "") {
+                winningNumSet.add(Integer.valueOf(winningNums[i]));
+            }
+        }
+
+        // card numbers
+        String[] cardNumbers = lottoCard.substring(cardNumbersIndex).strip().split(" ");
+        for (int i = 0; i < cardNumbers.length; i++) {
+            if (cardNumbers[i] != "") {
+                if (winningNumSet.contains(Integer.valueOf(cardNumbers[i]))) {
+                    numMatches++;
+                    // cardPoints = (cardPoints == 0) ? 1 : cardPoints << 1; // 1st star
+                    // System.out.println(totalPoints);
+                    // System.out.print(Integer.valueOf(cardNumbers[i]) + " ");
+                }
+            }
+        }
+        // totalPoints += cardPoints; // 1st star
+        // System.out.println();
+        return numMatches;
+    }
 
     public void compute() {
-        try (Scanner sc = new Scanner(new File(".\\Day Four.txt"))) {
-            while (gameRunning) {}
-            //while (sc.hasNextLine()) { // 1st star
-                // scanning the ticket
-                //int cardPoints = 0; // 1st star
+        try (Scanner sc = new Scanner(new File("src\\inputs\\Day Four.txt"))) {
+            // while (sc.hasNextLine()) { // 1st star
+            // int cardPoints = 0; // 1st star
+
+            // scanning the input file
+            while (sc.hasNextLine()) {
                 gameNumber++;
-                int numMatches = 0;
-                String lottoTicket = sc.nextLine();
-
-                // making dictionary for amt of times card is drawn
-                if (!numCards.containsKey(lottoTicket)) {
-                    numCards.put(lottoTicket, numMatches);
-                }
-                else {
-                    numCards.put(lottoTicket, numMatches + 1);
-                }
-                int winningNumbersIndex = lottoTicket.indexOf(":") + 1;
-                int ticketNumbersIndex = lottoTicket.indexOf("|") + 1;
-                
-                // winning numbers
-                String[] winningNums = lottoTicket.substring(winningNumbersIndex, ticketNumbersIndex - 1).strip().split(" ");
-                // TODO: get rid of the "" elements in winningNums before loop
-                for (int i = 0; i < winningNums.length; i++) {
-                    if (winningNums[i] != "") {
-                        winningNumSet.add(Integer.valueOf(winningNums[i]));
-                    }
-                }
-
-                // card numbers
-                String[] ticketNumbers = lottoTicket.substring(ticketNumbersIndex).strip().split(" ");
-                for (int i = 0; i < ticketNumbers.length; i++) {
-                    if (ticketNumbers[i] != "") {
-                        if (winningNumSet.contains(Integer.valueOf(ticketNumbers[i]))) {
-                            numMatches++;
-                            //cardPoints = (cardPoints == 0) ? 1 : cardPoints << 1; // 1st star
-                            //System.out.println(totalPoints);
-                            //System.out.print(Integer.valueOf(ticketNumbers[i]) + " ");
-                        }
-                    }
-                }
-                //totalPoints += cardPoints; // 1st star
-                winningNumSet.clear();
-                //System.out.println();
+                String lottoCard = sc.nextLine();
+                dataSet.put(gameNumber, scanCard(lottoCard));
             }
-            gameRunning = false;
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
-        System.out.println("Total points: " + totalPoints);
-    }    
+        /*
+         * 2nd star testing code
+         * int count = 0;
+         * while (count < 10) {
+         * count++;
+         */
+
+        int card = 1;
+        int matches = 0;
+        do {
+            matches = dataSet.get(card);
+            if (numCards.keySet().contains(card)) {
+                numCards.put(card, numCards.get(card) + 1);
+            } else {
+                numCards.put(card, 1);
+            }
+            System.out.println("Card: " + card + " Matches: " + matches);
+            System.out.println(numCards.get(card) + " loops");
+            for (int i = 0; i < numCards.get(card); i++) {
+                // System.out.println("Loop # " + (i + 1));
+                for (int j = 1; j <= matches; j++) {
+                    if (numCards.keySet().contains(card + j)) {
+                        numCards.put(card + j, numCards.get(card + j) + 1);
+                    } else {
+                        numCards.put(card + j, 1);
+                    }
+                }
+            }
+            System.out.println();
+            card++;
+        } while (dataSet.get(card) != null);
+        /*
+         * cardQueue.add(1);
+         * while (!cardQueue.isEmpty()) {
+         * 
+         * int card = cardQueue.remove(0);
+         * int matches = dataSet.get(card);
+         * if ((card + matches) > dataSet.size()) {
+         * matches = dataSet.size();
+         * }
+         * if (numCards.keySet().contains(card)) {
+         * numCards.put(card, numCards.get(card) + 1);
+         * } else {
+         * numCards.put(card, 1);
+         * }
+         * 
+         * System.out.println("Card # " + card + " has " + matches + " matches.");
+         * for (int i = 1; i <= matches; i++) {
+         * System.out.println("Adding card " + (card + i) + " to queue.");
+         * cardQueue.add(card + i);
+         * }
+         * Collections.sort(cardQueue);
+         * System.out.println("There are " + cardQueue.size() + " cards in queue.");
+         * }
+         */
+
+        // System.out.println("Total points: " + totalPoints); // 1st star
+        for (int a : numCards.keySet()) {
+            System.out.println("Number of " + a + " instances: " + numCards.get(a));
+            totalCards += numCards.get(a);
+        }
+        System.out.println("Total cards: " + totalCards);
+    }
 }
