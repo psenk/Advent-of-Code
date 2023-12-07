42155140aa2a58298eaf9af96ff427e42e59a59d
Paul Senkbeil <psenk168@gmail.com>
2023-12-06 20:45:19 -0600

    .

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //Day day = new DayOne();
+        //Day day = new DayTwo();
+        //Day day = new DayThree();
+        Day day = new DayFour();
+        day.compute();
+    }
+}
