3ea7e151c210db7f2b800c3df521786ba5d34c29
Paul Senkbeil <psenk168@gmail.com>
2023-12-12 23:26:48 -0600

    Day Nine prep

---
+++ src/DayNine.java
@@ -0,0 +1,9 @@
+public class DayNine implements Day {
+
+    @Override
+    public void compute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'compute'");
+    }
+    
+}
